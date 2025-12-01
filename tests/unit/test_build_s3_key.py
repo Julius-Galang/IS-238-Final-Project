65cb8126aaef27c73125cef7c0e42214513baadd
Angelo dela Fuente <aadelafuente1@up.edu.ph>
2025-12-02 03:08:16 +0800

    Update test_build_s3_key.py

--- tests/unit/test_build_s3_key.py
+++ tests/unit/test_build_s3_key.py
@@ -4,7 +4,7 @@ import sys
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parents[2]
 if str(ROOT_DIR) not in sys.path:
-    sys.path.insert(0.str(ROOT_DIR))
+    sys.path.insert(0, str(ROOT_DIR))
 
 from lambda_functions import _build_s3_key
 
